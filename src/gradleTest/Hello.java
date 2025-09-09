cd21114d83d6d0ed43bcd331b72636e5074c27bc
shkang <xxkshxx@gmail.com>
2025-09-09 10:07:56 +0900

    Update Hello.java
    ㅁㄴㅇㄻㅇㄻㅇㄴㄻㅇㄹ

--- src/gradleTest/Hello.java
+++ src/gradleTest/Hello.java
@@ -2,12 +2,13 @@ package gradleTest;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("1212122");
+		System.out.println("1212123331ㅁㅇㅁㅇㄻㅇㄹ2");
 
 
 
 
-		System.out.println("1212122");
+		System.out.println("121ㅇㄻㅇㄻㅇㄻㅇㄹ2122");
 	}
 }
+
 
