20f7f40fd7147b7c6c2412d574462cbc650602a0
shkang <xxksh@ksh>
2025-09-09 10:04:32 +0900

    adfadfadf

--- src/gradleTest/Hello.java
+++ src/gradleTest/Hello.java
@@ -3,5 +3,14 @@ package gradleTest;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("1212122");
+		System.out.println("1212122");
+		System.out.println("1212122");
+		System.out.println("1212122");
+		System.out.println("1212122");
+		System.out.println("1212122");
+		System.out.println("1212122");
+		System.out.println("1212122");
+		System.out.println("1212122");
+		System.out.println("1212122");
 	}
 }
